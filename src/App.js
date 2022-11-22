68ed6b0632c1e4cea3fb457985ea85afffd00b82
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-22 15:22:49 +0100

    add app js to hold the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Link to="/greeting">Go page details</Link>
+      <Routes>
+        <Route path="/greeting" element={<Greeting />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
